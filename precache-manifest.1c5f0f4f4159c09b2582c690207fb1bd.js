40f869179951dd2d62b360d699b34734bbb9bc55
Pratik Chauhan <pratikc.multiqos@gmail.com>
2022-03-24 10:35:49 +0530

    Updates

---
+++ precache-manifest.1c5f0f4f4159c09b2582c690207fb1bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d170077a592310612d3f",
+    "url": "/ReduxCart/static/css/main.84ec9949.chunk.css"
+  },
+  {
+    "revision": "d170077a592310612d3f",
+    "url": "/ReduxCart/static/js/main.4404c540.chunk.js"
+  },
+  {
+    "revision": "f3409fb5194dbc3473b4",
+    "url": "/ReduxCart/static/js/runtime~main.178ef938.js"
+  },
+  {
+    "revision": "c96cc519e6d3156924f4",
+    "url": "/ReduxCart/static/js/2.6056c8db.chunk.js"
+  },
+  {
+    "revision": "aa8ef4843812cf73606d9ac126adf482",
+    "url": "/ReduxCart/static/media/logo2.aa8ef484.png"
+  },
+  {
+    "revision": "345edf1ad18331871b72fdcf4e881be6",
+    "url": "/ReduxCart/static/media/banner2.345edf1a.jpg"
+  },
+  {
+    "revision": "822e5dc6ccec91bc679738e87fd7b1f0",
+    "url": "/ReduxCart/index.html"
+  }
+];
